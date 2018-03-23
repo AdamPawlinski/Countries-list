1a8ff67ff33a986020a7ca7684611ae3deb60b69
Adam Pawliński <adam.pawlinski@gmail.com>
2018-03-23 22:24:44 +0100

    finish task-12-6

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var url = 'https://restcountries.eu/rest/v2/name/';
+var countriesList = $('#countries');
+function searchCountries(){
+  var countryName = $('#country-name').val();
+  if(!countryName.length) countryName = 'Poland';
+  $.ajax({
+    dataType: "json",
+    url: url + countryName,
+    success: showCountriesList
+  });
+}
+
+function showCountriesList(resp){
+  countriesList.empty();
+  resp.forEach(function(item) {
+    $('<li>').text(item.name).appendTo(countriesList);
+  })
+}
+
+$('#search').on('click', searchCountries);
