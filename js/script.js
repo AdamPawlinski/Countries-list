612e3eb56f8eee4b2349eb8f64c5e7374d458ab5
Adam Pawliński <adam.pawlinski@gmail.com>
2018-03-27 06:54:35 +0200

    bugs fixed

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
-var url = 'https://restcountries.eu/rest/v2/name/';
-var countriesList = $('#countries');
-function searchCountries(){
-  var countryName = $('#country-name').val();
-  if(!countryName.length) countryName = 'Poland';
-  $.ajax({
-    dataType: "json",
-    url: url + countryName,
-    success: showCountriesList
-  });
+$(function(){
+  var url = 'https://restcountries.eu/rest/v2/name/';
+  var countriesList = $('#countries');
+  function searchCountries(){
+    var countryName = $('#country-name').val();
+    if (!countryName.length) countryName = 'Poland';
+    $.ajax({
+      dataType: "json",
+      url: url + countryName,
+      success: showCountriesList
+    });
+  }
+
+  function showCountriesList(resp){
+    countriesList.empty();
+
+    resp.forEach(function(item) {
+      $('<li>').text(item.name + ', capital: ' + item.capital).appendTo(countriesList);
+    })
+  }
+
+  $('#search').on('click', searchCountries);
 }
-
-function showCountriesList(resp){
-  countriesList.empty();
-
-  resp.forEach(function(item) {
-    $('<li>').text(item.name + ', capital: ' + item.capital).appendTo(countriesList);
-  })
-}
-
-$('#search').on('click', searchCountries);
